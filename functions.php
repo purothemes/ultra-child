82b61308ed80f49e8afa77cdbfe2d21e48deab02
Andrew Misplon <amisplon@gmail.com>
2015-05-07 18:01:17 +0200

    Small change to CSS enqueue.
    Signed-off-by: Andrew Misplon <amisplon@gmail.com>

--- functions.php
+++ functions.php
@@ -1,11 +1,9 @@
 <?php
 
 /**
-* Enqueue styles.
-*/
-add_action( 'wp_enqueue_scripts', 'ultra_child_styles' );
-
-function ultra_child_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ) );
+ * Enqueue the parent theme stylesheet.
+ */
+function ultra_child_enqueue_parent_style() {
+    wp_enqueue_style( 'ultra-parent-style', get_template_directory_uri() . '/style.css' );
 }
+add_action( 'wp_enqueue_scripts', 'ultra_child_enqueue_parent_style', 8 );
