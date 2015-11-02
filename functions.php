f296ce5415a575484e858c255b0fd5e38eaa8a3b
Andrew Misplon <amisplon@gmail.com>
2015-11-02 11:52:00 +0200

    Small change to enqueue.
    Signed-off-by: Andrew Misplon <amisplon@gmail.com>

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 /**
  * Enqueue the parent theme stylesheet.
  */
-function ultra_child_enqueue_parent_style() {
+function ultra_child_scripts() {
     wp_enqueue_style( 'ultra-parent-style', get_template_directory_uri() . '/style.css' );
 }
-add_action( 'wp_enqueue_scripts', 'ultra_child_enqueue_parent_style', 8 );
+add_action( 'wp_enqueue_scripts', 'ultra_child_scripts', 8 );
