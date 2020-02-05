4df6d5e69b16c5df9b418d82c90d68de923df445
Andrew Misplon <amisplon@gmail.com>
2020-02-05 18:12:20 +0200

    Minor formatting updates

--- functions.php
+++ functions.php
@@ -4,6 +4,6 @@
  * Enqueue the parent theme stylesheet.
  */
 function ultra_child_scripts() {
-    wp_enqueue_style( 'ultra-parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'ultra-parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'ultra_child_scripts', 8 );
